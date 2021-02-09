1a7d40cbfecb3ef603f6de177a387fbb72f77faf
Ewelina <eweszm@gmail.com>
2021-02-09 20:12:18 +0100

    Add `CityDetailsPresentationData`
    Add display model and update converters.

---
+++ Presentation/CityDetailsPresentationData.swift
@@ -0,0 +1,16 @@
+//
+//  CityDetailsPresentationData.swift
+//  Presentation
+//
+//  Created by Ewelina on 09/02/2021.
+//
+
+import Foundation
+
+public struct CityDetailsPresentationData {
+	public let title: String
+	public let imageUrl: URL?
+	public let visitorsCount: String
+	public let averageRating: String
+	public let favouritesText: String
+}
--- Presentation/Converters.swift
+++ Presentation/Converters.swift
@@ -14,3 +14,18 @@ public func convert(_ city: City) -> CityPresentationData {
 		indicator: city.isFavourite ? "♥︎" : "♡"
 	)
 }
+
+public func convert(_ city: CityDetails) -> CityDetailsPresentationData {
+	let visitorsCountText = NSLocalizedString("Visitors count: \(city.visitors.count) ➡️ℹ️", comment: "")
+	let averageRatingText = NSLocalizedString("Average rating: \(city.averageRating)", comment: "")
+	let addToFavouritesText = NSLocalizedString("Add to favourites", comment: "")
+	let removeFromFavouritesText = NSLocalizedString("Remove from favourites", comment: "")
+
+	return CityDetailsPresentationData(
+		title: city.name,
+		imageUrl: city.imageUrl,
+		visitorsCount: visitorsCountText,
+		averageRating: averageRatingText,
+		favouritesText: city.isFavourite ? removeFromFavouritesText : addToFavouritesText
+	)
+}
