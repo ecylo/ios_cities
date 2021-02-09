b75928b8cff3b802983c8a392f7f4ea11a50ebef
Ewelina <eweszm@gmail.com>
2021-02-09 16:19:05 +0100

    Add `CityResponse`
    Add response structure reflecting JSON response from backend.

---
+++ Platform/CityResponse.swift
@@ -0,0 +1,13 @@
+//
+//  CityResponse.swift
+//  Platform
+//
+//  Created by Ewelina on 09/02/2021.
+//
+
+import Foundation
+
+struct CityResponse: Decodable {
+	let name: String
+	let imageUrl: String
+}
